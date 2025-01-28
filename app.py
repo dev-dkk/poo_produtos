62c5c4d4e12f36e53b3835801669da4b6a79d051
Dérick <santos.c.derick@gmail.com>
2025-01-27 22:51:43 -0300

    Todas as funcoes de gerenciamento do banco de dados foram adicionadas

--- app.py
+++ app.py
@@ -23,15 +23,35 @@ class Produto(Base):
 #Classe gerenciamento banco de dados
 
 class ProdutoDB:
+    #Metodo construtor
     def __init__(self):
         self.engine = create_engine('sqlite:///produto.db')
         Base.metadata.create_all(self.engine)
         self.Session = sessionmaker(bind=self.engine)
     
-    def adc_produto(self, nome, preco_entrada, preco_saida, taxa_aumento, quantidade):
+    #Metodo adicionar Protudos
+    def adc(self, nome, preco_entrada, preco_saida, taxa_aumento, quantidade):
         session = self.Session()
         produto = Produto(nome = nome, preco_entrada = preco_entrada, preco_saida = preco_saida, taxa_aumento = taxa_aumento, quantidade =
                           quantidade)
         session.add(produto)
         session.commit()
         session.close()
+    
+    #Metodo Listar Produtos
+    def lst(self):
+        session = self.Session()
+        produtos = session.query(Produto).all()
+        session.close()
+        return produtos
+    
+    #Metodo Deletar Produtos
+    def dele(self, produto_id):
+        session = self.Session()
+        produto = session.query(Produto).get(produto_id)
+        if produto:
+            session.delete(produto)
+            session.commit()
+        session.close()
+
+    
