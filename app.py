07f1fc9fcf797f663c1be48285d5828d768d13d6
Dérick <santos.c.derick@gmail.com>
2025-01-27 22:40:00 -0300

    Configuracao do banco de dados

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+#Importacao de bibliotecas
+
 from flet import Page, TextField, ElevatedButton, DataTable, DataRow, DataColumn, DataCell, Text, icons, IconButton
 from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.orm import sessionmaker, declarative_base
+
+#Configuracao do Banco de dados
 
 Base = declarative_base()
 
@@ -13,4 +17,6 @@ class Produto(Base):
     nome = Column(Integer, nullable=False)
     preco_entrada = Column(Float, nullable=False)
     preco_saida = Column(Float)
+    taxa_aumento = Column(Float, nullable=False)
     quantidade = Column(Integer, nullable=False)
+
